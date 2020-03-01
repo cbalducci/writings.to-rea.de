79f6c92b48051f1102e151640c356e7118d432bc
Cristiano Balducci <cristiano.balducci@gmail.com>
2020-03-01 11:33:41 +0000

    remove blogroll and social

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Cristiano Balducci'
 SITENAME = 'Writings'
-SITEURL = ''
+SITEURL = 'https://writings.to-rea.de'
 
 PATH = 'content'
 
@@ -19,17 +19,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 1
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
